fc028600529c8e1257cf5153d939d2cecf18abba
zhanghuadongzi@hotmail.com <zhanghuadongzi0506>
2016-05-31 16:00:55 +0800

    New changelist

---
+++ app/src/main/java/com/dongzi/cartoon/ui/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.dongzi.cartoon.ui;
+
+
+import android.os.Bundle;
+import com.dongzi.cartoon.R;
+
+public class RegisterActivity extends BaseFrameWorkActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
