fcb5ac33bc9ec64cc67c17d66183b70c424aeb48
zhanghuadongzi@hotmail.com <zhanghuadongzi0506>
2016-05-31 16:40:35 +0800

    New changelist

---
+++ app/src/main/java/com/dongzi/cartoon/ui/SettingActivity.java
@@ -0,0 +1,44 @@
+package com.dongzi.cartoon.ui;
+
+import android.annotation.TargetApi;
+import android.app.FragmentManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.dongzi.cartoon.R;
+
+public class SettingActivity extends BaseFrameWorkActivity {
+
+    private SettingsFragment mSettingsFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+        if (savedInstanceState == null) {
+            mSettingsFragment = new SettingsFragment();
+            replaceFragment(R.id.fl_setting, mSettingsFragment);
+        }
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public void replaceFragment(int viewId, android.app.Fragment fragment) {
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(viewId, fragment).commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+}
