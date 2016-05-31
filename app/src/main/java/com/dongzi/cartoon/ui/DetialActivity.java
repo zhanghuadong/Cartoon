ad46f58ef47d5e954214d78402d0ab55cbde226d
zhanghuadongzi@hotmail.com <zhanghuadongzi0506>
2016-05-31 18:30:11 +0800

    New changelist

---
+++ app/src/main/java/com/dongzi/cartoon/ui/DetialActivity.java
@@ -0,0 +1,15 @@
+package com.dongzi.cartoon.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dongzi.cartoon.R;
+
+public class DetialActivity extends BaseFrameWorkActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detial);
+    }
+}
